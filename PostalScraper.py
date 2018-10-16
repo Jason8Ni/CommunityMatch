838c1c67a175898494db59208ad368c8952e529f
jason8ni <jason8ni@hotmail.com>
2018-10-15 21:19:20 -0400

    complete scraper

--- PostalScraper.py
+++ PostalScraper.py
@@ -1,23 +1,22 @@
 
 # coding: utf-8
 
-# In[9]:
+# In[158]:
 
 
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 import requests
-import urllib2.open
 
 
-# In[41]:
+# In[159]:
 
 
 postalData = pd.DataFrame(columns = ["PostalCode", "Borough", "Neighbourhood"])
 
 
-# In[19]:
+# In[160]:
 
 
 obj  = requests.get("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M")
@@ -27,59 +26,131 @@ page = obj.text
 soup = BeautifulSoup(page, "lxml")
 
 
-# In[17]:
+# In[161]:
 
 
 print(soup)
 
 
-# In[23]:
+# In[162]:
 
 
 dataTable = soup.find('table', class_='wikitable sortable')
 
 
-# In[25]:
+# In[163]:
 
 
 row = dataTable.find('tr')
 
 
-# In[31]:
+# In[164]:
 
 
 print(row)
 
 
-# In[29]:
+# In[165]:
 
 
 rowData = row.find_all('th')
 
 
-# In[30]:
+# In[166]:
 
 
 print(rowData)
 
 
-# In[55]:
+# In[167]:
+
+
+print(dataTable.find_all('tr'))
+
+
+# In[168]:
 
 
 
 
-for row in dataTable.find_all('tr'):
+for row in dataTable.find_all('tr')[1:]:
     temp = []
     for cell in row.find_all('td'):
         temp.append(cell.text)
         #print(cell.text)
-        print(temp)
-    postalData = postalData.append(pd.DataFrame(temp).T)
-    #print()
+    print(temp)
+    postalData = postalData.append(
+    dict(zip(["PostalCode", "Borough", "Neighbourhood"], temp)), ignore_index=True)
 
 
-# In[53]:
+# In[169]:
+
+
+postalData = postalData[postalData.Borough != "Not assigned"]
+postalData["Neighbourhood"] = postalData["Neighbourhood"].replace({'\n':''}, regex=True)
+postalData=postalData.reset_index()
+
+
+# In[173]:
 
 
 print(postalData)
+
+
+# In[175]:
+
+
+postalData = postalData.drop(["index"], axis = 1)
+
+
+# In[176]:
+
+
+print(postalData)
+
+
+# In[177]:
+
+
+postalData.set_value(6,'Neighbourhood', "Queen's Park")
+
+
+# In[178]:
+
+
+def concatenate_neighbourhood(x):
+    cadena = ""
+    for i in range(len(x)-1):
+        cadena = cadena + x.iloc[i] + ", "
+    cadena += x.iloc[-1]
+    return cadena
+
+def select_Borough(x):
+    ref = x.iloc[0]
+    for i in range(1, len(x)):
+        if ref != x.iloc[i]:
+            for i in x:
+                print(x)
+            raise Exception("Postcode comprises two Boroughs")
+    return ref
+
+
+# In[180]:
+
+
+postDataFiltered = postalData.groupby(["PostalCode"]).agg({"Borough": lambda x: select_Borough(x),
+                                 "Neighbourhood": lambda x: concatenate_neighbourhood(x)})
+postDataFiltered.head()
+
+
+# In[182]:
+
+
+postDataFiltered
+
+
+# In[183]:
+
+
+postDataFiltered.shape
 
